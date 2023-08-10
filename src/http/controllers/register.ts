1975733382a28ac7daa5f9e1ef9a44d6bccfa3a9
Andre Alves <andresistinfo@gmail.com>
2023-08-10 06:49:46 +0100

    Creating user password hash

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,4 +1,5 @@
 import { prisma } from '@/lib/prisma'
+import bcryptjs from 'bcryptjs'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 
@@ -11,8 +12,20 @@ export async function register(req: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(req.body)
 
+  const password_hash = await bcryptjs.hash(password, 6)
+
+  const userWithSameEmail = await prisma.user.findUnique({ where: { email } })
+
+  if (userWithSameEmail) {
+    return reply.status(409).send()
+  }
+
   await prisma.user.create({
-    data: { name, email, password_hash: password },
+    data: {
+      name,
+      email,
+      password_hash,
+    },
   })
 
   return reply.status(201).send()

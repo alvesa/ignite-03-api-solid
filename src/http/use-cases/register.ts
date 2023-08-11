f4596f712ace53627536a28b5cfea062384b86b5
Andre Alves <andresistinfo@gmail.com>
2023-08-11 06:31:46 +0100

    Creating new repository

--- src/http/use-cases/register.ts
+++ src/http/use-cases/register.ts
@@ -1,5 +1,6 @@
 import { prisma } from '@/lib/prisma'
 import bcryptjs from 'bcryptjs'
+import { PrismaUsersRepository } from '../repositories/prisma-users-repository'
 
 interface RegisterUseCaseRequest {
   name: string
@@ -20,11 +21,11 @@ export async function registerUseCase({
     throw new Error('Email already exists')
   }
 
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash,
-    },
+  const prismaUsersRepository = new PrismaUsersRepository()
+
+  await prismaUsersRepository.create({
+    name,
+    email,
+    password_hash,
   })
 }

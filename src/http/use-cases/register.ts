2e163eec26360b793c6556fe89ee2c8109676968
Andre Alves <andresistinfo@gmail.com>
2023-08-11 07:00:04 +0100

    Removing prisma dependecy inside use case

--- src/http/use-cases/register.ts
+++ src/http/use-cases/register.ts
@@ -1,6 +1,5 @@
-import { prisma } from '@/lib/prisma'
 import bcryptjs from 'bcryptjs'
-import { PrismaUsersRepository } from '../repositories/prisma-users-repository'
+import { UsersRepository } from '../repositories/users-repository'
 
 interface RegisterUseCaseRequest {
   name: string
@@ -9,12 +8,12 @@ interface RegisterUseCaseRequest {
 }
 
 export class RegisterUseCase {
-  constructor(private usersRepository: any) {}
+  constructor(private usersRepository: UsersRepository) {}
 
   async execute({ name, email, password }: RegisterUseCaseRequest) {
     const password_hash = await bcryptjs.hash(password, 6)
 
-    const userWithSameEmail = await prisma.user.findUnique({ where: { email } })
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
     if (userWithSameEmail) {
       throw new Error('Email already exists')

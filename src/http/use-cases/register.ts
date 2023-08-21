ce2e7789246d143f4b5727dfdd80a5110e6babf3
Andre Alves <andresistinfo@gmail.com>
2023-08-21 06:04:59 +0100

    Including error handling

--- src/http/use-cases/register.ts
+++ src/http/use-cases/register.ts
@@ -1,5 +1,6 @@
 import bcryptjs from 'bcryptjs'
 import { UsersRepository } from '../repositories/users-repository'
+import { UserAlreadyExistsError } from './errors/user-already-exists'
 
 interface RegisterUseCaseRequest {
   name: string
@@ -16,7 +17,7 @@ export class RegisterUseCase {
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
     if (userWithSameEmail) {
-      throw new Error('Email already exists')
+      throw new UserAlreadyExistsError()
     }
 
     await this.usersRepository.create({

e08b1c9c9c4905abc6523f1800319e1be1af1a2c
Andre Alves <andresistinfo@gmail.com>
2023-08-25 06:19:13 +0100

    Fixing false positive test

--- src/http/use-cases/register.spec.ts
+++ src/http/use-cases/register.spec.ts
@@ -47,7 +47,7 @@ describe('Register Use Case', () => {
       password: '123456789',
     })
 
-    expect(() =>
+    await expect(() =>
       registerUseCase.execute({
         name: 'John Doe',
         email,

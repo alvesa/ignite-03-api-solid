b2c6b302434c12ad0ee4da8813bca503600df211
Andre Alves <andresistinfo@gmail.com>
2023-08-30 06:39:01 +0100

    Added counter of checkins

--- src/http/repositories/check-ins-repository.ts
+++ src/http/repositories/check-ins-repository.ts
@@ -4,4 +4,5 @@ export interface CheckInsRepository {
   create(data: Prisma.CheckinUncheckedCreateInput): Promise<Checkin>
   findManyByUserId(userId: string, page: number): Promise<Checkin[]>
   findByUserIdOnDate(userId: string, date: Date): Promise<Checkin | null>
+  countByUserId(userId: string): Promise<number>
 }

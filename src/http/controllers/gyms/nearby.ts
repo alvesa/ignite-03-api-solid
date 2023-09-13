e8ed71f534c7bb3d741d4facd24e921dee590a3a
Andre Alves <andresistinfo@gmail.com>
2023-09-13 06:25:45 +0100

    Created gyms routes and controllers

---
+++ src/http/controllers/gyms/nearby.ts
@@ -0,0 +1,25 @@
+import { makeFeatchNearbyGymsUseCase } from '@/use-cases/factories/make-featch-nearby-gyms-use-case'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function nearby(req: FastifyRequest, reply: FastifyReply) {
+  const nearbyGymQuerySchema = z.object({
+    latitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90
+    }),
+    longitude: z.number().refine((value) => {
+      return Math.abs(value) <= 180
+    }),
+  })
+
+  const { latitude, longitude } = nearbyGymQuerySchema.parse(req.body)
+
+  const searchGymsUseCase = makeFeatchNearbyGymsUseCase()
+
+  const { gyms } = await searchGymsUseCase.execute({
+    userLatitude: latitude,
+    userLongitude: longitude,
+  })
+
+  return reply.status(200).send({ gyms })
+}
---
+++ src/http/controllers/gyms/search.ts
@@ -0,0 +1,21 @@
+import { makeSearchGymsUseCase } from '@/use-cases/factories/make-search-gyms-use-case'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function search(req: FastifyRequest, reply: FastifyReply) {
+  const searchGymQuerySchema = z.object({
+    q: z.string(),
+    page: z.coerce.number().min(1).default(1),
+  })
+
+  const { q, page } = searchGymQuerySchema.parse(req.body)
+
+  const searchGymsUseCase = makeSearchGymsUseCase()
+
+  const { gyms } = await searchGymsUseCase.execute({
+    query: q,
+    page,
+  })
+
+  return reply.status(200).send({ gyms })
+}
